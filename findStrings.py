1cb6673b2da73e5ba5a41b53b907e381e0c905a6
saro6971 <sam.ronnlund@gmail.com>
2015-10-24 00:32:55 +0200

    nu

--- findStrings.py
+++ findStrings.py
@@ -5,18 +5,19 @@ from celery import Celery
 
 @celery.task
 def pronounCount(tweetfile):
-    triggerWords = {"han" : 0, "hon" : 0, "den" : 0,
-                    "denna" : 0, "denne" : 0, "hen" : 0}
+    triggerWords = {"han" : 0, "hon" : 0, "den" : 0
+                   , "hen" : 0 ,"denna" : 0, "denne" : 0}
     key = triggerWords.keys()
     
     with open(tweetfile,"r") as f:                     #read the file// make this generic for all files
         jsonDataLines = f.readlines()                             #read all the lines in file f
         for data in jsonDataLines:                                #for every line in all the lines
-            if data != '\n':                                      #disregard empty lines
+            if data != '\n' and "retweeted_status" not in data:                                      #disregard empty lines and retweets
                 json_data = json.loads(data)                      #load data as json
                 text = json_data["text"].split()                  #save word for word in text-list
                 for words in text:                                #
                     if words.lower() in key:                      #for every word which corresponds to a key
                         triggerWords[words.lower()] += 1          # add 1 to that value
-                        
+ #   print triggerWords                    
     return triggerWords
+#pronounCount("/home/sam/Desktop/tweets_19.txt.part");
--- main.py
+++ main.py
@@ -7,21 +7,15 @@ from driver import app
 
 @app.route('/', methods=['GET'])
 def tweetCountAll():
+    for x in range(0,20):
+        tasks = pronounCount.delay("tweets_"+ str(x) + ".txt")
+    
+    while(tasks.ready() == False):
+        time.sleep(4)
 
-    queue = [pronounCount.s('tweets_{}.txt'.format(x)) for x in xrange(0,20)]
-    g = group(queue)
-
-    res = g()
-
-    while (res.ready() == False):
-        time.sleep(3)
-
-    dicts = res.get()
-    #counter = Counter()
-    #for dic in dicts:
-      #  counter.update(dic)
-
-    return dicts
+    res = tasks.get()
+    return res
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)

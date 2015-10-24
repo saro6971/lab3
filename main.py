3af9a23307f67bbd0a0a5534264bac6c2b38562a
saro6971 <sam.ronnlund@gmail.com>
2015-10-24 14:16:49 +0200

    NUDA

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import time
-from findStrings import pronounCount
+from findStrings import pronounCount,app
 from celery import group, subtask
-from flask import jsonify
-from collections import Counter
-from driver import app
+#from flask import jsonify
+#from collections import Counter
+#from driver import app
 
-@app.route('/', methods=['GET'])
+#@app.route('/', methods=['GET'])
+@app.task()
 def tweetCountAll():
     for x in range(0,20):
         tasks = pronounCount.delay("tweets_"+ str(x) + ".txt")
     #tasks = pronounCount.delay("/home/sam/Desktop/tweets_19.txt.part")
-    print "ja"
+    #print "ja"
 
     while(tasks.ready() == False):
         time.sleep(4)
 
     res = tasks.get()
     #print res
-    counter = Counter()
-    for dic in res:
-        counter.update(dic)
+    #counter = Counter()
+    #for dic in res:
+    #    counter.update(dic)
 
-    return jsonify(dict(counter)), 200
+    return jsonify(res)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = True)
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', debug = True)

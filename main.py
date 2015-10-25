bf5835431844901952511f074bc2f7b0b9b37fda
saro6971 <sam.ronnlund@gmail.com>
2015-10-25 17:02:49 +0100

    s

--- main.py
+++ main.py
@@ -5,29 +5,25 @@ from flask import jsonify
 from collections import Counter
 from driver import app
 
-
+#tasks = pronounCount.delay("/home/sam/Desktop/tweets_19.txt.part")
+    #print "ja"
 #@app.task()
 @app.route('/', methods=['GET'])
 def tweetCountAll():
+triggerWordsTot = {"han" : 0, "hon" : 0, "den" : 0
+                   , "hen" : 0 ,"denna" : 0, "denne" : 0}
     for x in xrange(0,3):
         tasks = pronounCount.delay("tweets_"+ str(x) + ".txt")
-    #tasks = pronounCount.delay("/home/sam/Desktop/tweets_19.txt.part")
-    #print "ja"
-
-    while(tasks.ready() == False):
-        time.sleep(2)
-
-    result = tasks.get()
-    print result
-    triggerWordsTot = {"han" : 0, "hon" : 0, "den" : 0
-                   , "hen" : 0 ,"denna" : 0, "denne" : 0}
-    triggerWordsTot["han"] += result["han"]
-    triggerWordsTot["hon"] += result["hon"]
-    triggerWordsTot["den"] += result["den"]
-    triggerWordsTot["det"] += result["det"]
-    triggerWordsTot["hen"] += result["hen"]
-    triggerWordsTot["denna"] += result["denna"]
-    triggerWordsTot["denne"] += result["denne"]
+        while(tasks.ready() == False):
+            time.sleep(1)
+        result = tasks.get()
+        triggerWordsTot["han"] += result["han"]
+        triggerWordsTot["hon"] += result["hon"]
+        triggerWordsTot["den"] += result["den"]
+        triggerWordsTot["det"] += result["det"]
+        triggerWordsTot["hen"] += result["hen"]
+        triggerWordsTot["denna"] += result["denna"]
+        triggerWordsTot["denne"] += result["denne"]
     #for dictionary in result:
     #    for key in dictionary:
     #        triggerWordsTot[key] += dictionary[key]
